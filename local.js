93f07aa075e81a6b657e7a362c33aa9b37e878e9
andradedaniel14 <andrade.suarez@hotmail.com>
2023-07-10 18:31:42 -0500

    script local scope

---
+++ local.js
@@ -0,0 +1,21 @@
+// Local scope
+// function greeting(){
+//     var user_name = 'daniel'
+//     return user_name
+// }
+
+// if(user_name === 'David'){
+//     console.log('Hola Daniel')
+// } else{
+//     console.log('No son iguales')
+// }
+
+//Buen ejemplo
+function miFruta(){
+    var fruta = 'manzana'
+    if (fruta === 'manzana'){
+        return `Mi fruta favorita es ${fruta}`
+    }
+}
+
+console.log(miFruta());
