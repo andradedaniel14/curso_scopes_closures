04b11a6c7799893b0831b36816c9aa7a2476b735
andradedaniel14 <andrade.suarez@hotmail.com>
2023-07-10 18:20:22 -0500

    script scope global

---
+++ global.js
@@ -0,0 +1,15 @@
+//Variables
+
+var a //declaracion
+var b = 'b' //asignacion
+b = 'bb' // reasignacion
+var a = 'aa' // redeclaracion
+
+//Global scope
+var fruta = 'pera'
+
+function miFruta(){
+    console.log(fruta)
+}
+
+miFruta()
