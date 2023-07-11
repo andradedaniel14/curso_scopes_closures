81d9a667960111350fd91de13bf5f988b041fd13
andradedaniel14 <andrade.suarez@hotmail.com>
2023-07-10 19:17:18 -0500

    script reasignacion y redeclaracion

---
+++ reaclaracion_reasignacion.js
@@ -0,0 +1,19 @@
+//var: Reasignacion y redeclaracion
+var var_1 = 'daniel'
+var_1 = 'david'
+console.log(var_1)
+
+var var_2 = 'laura'
+var var_2 = 'paula'
+console.log(var_2)
+
+// let: Reasignacion y redeclaracion (no se puede)
+let var_3 = 'alfredo'
+var_3 = 'rafa'
+console.log(var_3)
+
+//const: no se puede ninguna
+const var_4 = 'junior'
+const var_4 = 'gaby'
+var_4 = 'harry'
+console.log(var_4)
