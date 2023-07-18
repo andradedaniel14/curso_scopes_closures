b20a8842fbe480d921d247405dbc831d47a36d98
andradedaniel14 <andrade.suarez@hotmail.com>
2023-07-17 19:36:32 -0500

    Ejemplo de alcancia

--- closure.js
+++ closure.js
@@ -33,3 +33,23 @@ function sumWithClosure(firstNum) {
   }
 
 console.log(sumWithClosure(2)())
+
+//Ejemplo
+function moneyBox() {
+    let saveCoins = 0;
+    function countCoins(coins) {
+      saveCoins += coins;
+      console.log(`MoneyBox: $${saveCoins}`)
+    }
+    return countCoins;
+  }
+  
+  const myMoneyBox = moneyBox();
+  myMoneyBox(5);
+  myMoneyBox(5);
+  myMoneyBox(15);
+  
+  const moneyBoxAna = moneyBox();
+  moneyBoxAna(10);
+  moneyBoxAna(20);
+  moneyBoxAna(5);
