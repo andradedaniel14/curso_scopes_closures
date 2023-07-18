5b0816167a6d2e4fe3981464fb3bdd8ae07b7aa0
andradedaniel14 <andrade.suarez@hotmail.com>
2023-07-17 19:01:21 -0500

    Script JS de closures con ejemplo de playground

---
+++ closure.js
@@ -0,0 +1,35 @@
+//Closures sencillo
+let contador = 1
+function prueba(){
+    contador += 1
+    return contador
+}
+
+console.log(prueba())
+
+//Closure avanzado
+function miFuncion(){
+    const var_1 = 0
+    function miFuncionHija(){
+        return console.log(var_1)
+    }
+    return miFuncionHija()
+}
+miFuncion()
+
+//Creacion closure para sumar
+function sumWithClosure(firstNum) {
+    let num_1 = firstNum
+    function sumSecond(secondNum) {
+        let num_2 = secondNum
+      if (secondNum) { //A ser un vacio, JavaScript lo interpreta como un valor Falsy
+        return num_1 + num_2
+      } 
+      else {
+        return num_1
+      }
+    }
+    return sumSecond
+  }
+
+console.log(sumWithClosure(2)())
